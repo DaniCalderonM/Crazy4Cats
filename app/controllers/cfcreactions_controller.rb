2b9181ff856e1dfa5bd8d2d2b115060dcc3cc8fc
danicalderonm <mesiasd.calderon@gmail.com>
2023-07-26 14:24:39 -0400

    tercer commit

--- app/controllers/cfcreactions_controller.rb
+++ app/controllers/cfcreactions_controller.rb
@@ -18,32 +18,4 @@ class CfcreactionsController < ApplicationController
         end
         end
     end
-
-
-
-
-    # def new_cfcuser_cfcreaction
-    #     @cfcuser = current_cfcuser
-    #     @cfcpost = Cfcpost.find(params[:cfcpost_id]) 
-    #     if params[:cfcpost_id]
-    #     @kind = params[:kind]
-    #     respond_to do |format|
-
-    #         cfcreaction_cfcpost = Cfcreaction.find_by(cfcuser_id: @cfcuser.id, cfcpost_id: @cfcpost.id)
-    #         @cfcreaction = Cfcreaction.new(cfcuser_id: @cfcuser.id, cfcpost_id:
-    #         @cfcpost.id, kind: @kind)
-    #             if @cfcuser_id == @cfcuser_id
-    #                 format.html { redirect_to cfcpost_url(@cfcpost), notice: 'Ya reaccionaste a esta publicación!' }
-                
-    #             #Creacion de reaccion con user y post
-                
-    #             if @cfcreaction.save!
-    #             format.html { redirect_to cfcpost_url(@cfcpost), notice: "Reaccionaste con éxito!" }
-    #             else 
-    #             format.html { redirect_to cfcpost_url(@cfcpost), notice: "Algo salio mal" }
-    #             end
-    #         end
-    #     end
-    #     end
-    # end 
 end
--- app/models/cfcreaction.rb
+++ app/models/cfcreaction.rb
@@ -2,8 +2,7 @@ class Cfcreaction < ApplicationRecord
   belongs_to :cfcuser
   belongs_to :cfcpost, optional: true
 
-  # Kinds = %w[ like dislike ].freeze
-  # KindsSpanish = {"like" => "Me gusta", "dislike" => "No me gusta"}.freeze
+
   validates :kind, acceptance: {
     accept: %w[like dislike ]
     }
